7b5764110e87aafd8024690082e84fbca30755c7
Fitssum Haile <fitsum.wolde@gmail.com>
2017-06-07 12:02:37 -0400

    added img link in seed.rb

--- game_of_thrones/db/schema.rb
+++ game_of_thrones/db/schema.rb
@@ -10,17 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607142342) do
+ActiveRecord::Schema.define(version: 20170607154130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "characters", force: :cascade do |t|
+    t.string "char_name"
+    t.string "char_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "houses", force: :cascade do |t|
+    t.string "house_name"
+    t.string "kingdom"
+    t.string "house_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
